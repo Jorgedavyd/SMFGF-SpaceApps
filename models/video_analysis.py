dba6fd86a712242e159b16385aa6d8a2816a83da
Jorge Enciso <jorged.encyso@gmail.com>
2023-11-05 10:14:59 -0300

    all 3d pretrained nets set

--- models/video_analysis.py
+++ models/video_analysis.py
@@ -6,10 +6,14 @@ import torch
 import torch.nn.functional as F
 import torchvision.transforms as tt
 class GeoVideoCNNRNN(GeoBase):
-    def __init__(self, image_model, rnn):
+    def __init__(self, image_model, rnn, architecture, dropout = 0.2):
         super(GeoVideoCNNRNN, self).__init__()
         self.feature_extractor = image_model
         self.sequential_extractor = rnn
+        self.fc = nn.Sequential(
+            DeepNeuralNetwork(rnn.hidden_size, rnn.hidden_size/2, *architecture),
+            nn.Dropout(dropout, inplace = True)
+            )
     def forward(self, x):
         _, seq_length, _,_,_ = x.size()
         feature_extraction = []
@@ -26,19 +30,63 @@ class GeoVideoCNNRNN(GeoBase):
             _, hn = self.sequential_extractor(out)
         
         return hn
-class ThreeD_CNN(nn.Module):
-    def __init__(self, hidden_state_size, architecture):
+class MC3_18(nn.Module):
+    def __init__(self, hidden_state_size, architecture, dropout: float = 0.2):
         from torchvision.models.video import mc3_18, MC3_18_Weights
-        super(ThreeD_CNN, self).__init__()
+        super(MC3_18, self).__init__()
         
         self.model = mc3_18(weights = MC3_18_Weights.KINETICS400_V1)
 
         for param in self.model.parameters():
             param.requires_grad = False
 
-        self.model.fc = DeepNeuralNetwork(512, hidden_state_size, *architecture)
+        self.model.fc = nn.Sequential(
+            DeepNeuralNetwork(512, hidden_state_size, *architecture),
+            nn.Dropout(dropout, inplace = True)
+            )
 
         self.transform = tt.Compose([tt.ToTensor(), MC3_18_Weights.KINETICS400_V1.transforms(antialias=True)])
+    def forward(self, x):
+        hn = self.model(x)
+        return hn
+    
+
+class MVIT_V2_S(nn.Module):
+    def __init__(self, hidden_state_size, architecture, dropout: float = 0.2):
+        from torchvision.models.video import mvit_v2_s, MViT_V2_S_Weights
+        super(MVIT_V2_S, self).__init__()
+        
+        self.model = mvit_v2_s(weights = MViT_V2_S_Weights.KINETICS400_V1)
+
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        self.model.head = nn.Sequential(
+            DeepNeuralNetwork(768, hidden_state_size, *architecture),
+            nn.Dropout(dropout, inplace = True)
+            )
+
+        self.transform = tt.Compose([tt.ToTensor(), MViT_V2_S_Weights.KINETICS400_V1.transforms(antialias=True)])
+    def forward(self, x):
+        hn = self.model(x)
+        return hn
+
+class SWIN3D_B(nn.Module):
+    def __init__(self, hidden_state_size, architecture, dropout: float = 0.2):
+        from torchvision.models.video import swin3d_b, Swin3D_B_Weights
+        super(SWIN3D_B, self).__init__()
+        
+        self.model = swin3d_b(weights = Swin3D_B_Weights.KINETICS400_IMAGENET22K_V1)
+
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        self.model.head = nn.Sequential(
+            DeepNeuralNetwork(1024, hidden_state_size, *architecture),
+            nn.Dropout(dropout, inplace = True)
+            )
+
+        self.transform = tt.Compose([tt.ToTensor(), Swin3D_B_Weights.KINETICS400_IMAGENET22K_V1.transforms(antialias=True)])
     def forward(self, x):
         hn = self.model(x)
         return hn
