8e9502f3bbb22c39694f51dafe05053b6277aa6f
Jorge Enciso <jorged.encyso@gmail.com>
2023-11-30 15:24:07 -0300

    gpu

--- models/utils.py
+++ models/utils.py
@@ -36,7 +36,37 @@ class Attention(nn.Module):
         self.layer_norm = nn.LayerNorm(input_size)
     
     def forward(self, x):
-        
         attn, _ = self.attn(x,x,x)
         context = self.layer_norm(attn)
         return context
+    
+## GPU usage
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu') #utilizar la gpu si está disponible
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    #Determina el tipo de estructura de dato, si es una lista o tupla la secciona en su subconjunto para mandar toda la información a la GPU
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl:
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl) #Mandar los data_loader que tienen todos los batch hacia la GPU
